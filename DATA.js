64c80e9bbb89acccfb030085e19ce563e6dc4b99
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-28 16:00:47 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "9743e75f-cd83-4cf5-bb35-2d868192d501",
+    "home_feed": [
+        {
+            "id": "bedbc558-b26e-4dad-8dfa-ccdf16a0236b",
+            "artist_name": "probably",
+            "home_carousel": [
+                {
+                    "id": "bf3d214a-1cb1-44e5-a365-74301ee3f956",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "9225fe9d-7141-4fc4-9d0a-4acb6d38ac3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "c07ab00c-adfe-4e74-8eee-0ecca8cde790",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                },
+                {
+                    "id": "21fe8dd1-2b6e-4575-9638-d37f277f36d6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "41d9af12-295b-4029-9add-e19a5db6118f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                }
+            ],
+            "art_caption": "thus",
+            "art_description": "History safe career your pattern protect. Exactly side than account beautiful view over. Do type game their."
+        },
+        {
+            "id": "f8578335-d642-46e8-af4b-2da6faf25f59",
+            "artist_name": "south",
+            "home_carousel": [
+                {
+                    "id": "cd18ce0a-fce7-4f02-82d1-0cf5870a70d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "25680965-0559-4f20-8d69-165216233127",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "30a483bf-197e-462a-a9b8-35e1a831b177",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "d1189281-3f48-4c0d-bca0-0e8e72db926c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "bc0c729a-10bd-427c-94a0-0023526dd730",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                }
+            ],
+            "art_caption": "main",
+            "art_description": "Food first player. Identify year possible teacher."
+        },
+        {
+            "id": "c553968f-6ada-4ee8-8251-95f56cc9b2fb",
+            "artist_name": "million",
+            "home_carousel": [
+                {
+                    "id": "6d74c807-2243-43fa-909c-4b3b82a78fc5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "e3a0db94-de3b-42f1-8703-d9dbbe42996f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "5a24cd2a-c847-473f-9c16-67a571722d98",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "d9101781-37ff-452e-96a0-2ff2b9f47f2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                },
+                {
+                    "id": "b3d4a0c1-a42b-41c6-bcb1-abd6cdf40c14",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                }
+            ],
+            "art_caption": "so",
+            "art_description": "Including eight face budget house manage. Environment soldier election TV task herself."
+        },
+        {
+            "id": "0a1d0f98-2d3a-4e82-864d-2171b636eb8d",
+            "artist_name": "anything",
+            "home_carousel": [
+                {
+                    "id": "6f5ae424-259b-49b8-b74a-1fedaa576294",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "292099ec-1665-4662-aa60-44812876c8e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "03ea82ae-c21f-41f5-b399-e2215f7b79b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                },
+                {
+                    "id": "774d49e6-701b-4b1c-9958-522afc03b61f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "ef9977c1-f16c-45b6-baf7-553b15af6699",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png"
+                }
+            ],
+            "art_caption": "record",
+            "art_description": "Voice cut process similar high once. Probably put result political win oil head."
+        },
+        {
+            "id": "d1b8c4d0-ebb2-4a36-93a3-49e23704dcae",
+            "artist_name": "single",
+            "home_carousel": [
+                {
+                    "id": "739921ba-fee7-4e64-a768-eae27845367a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "3125912e-6be8-4720-b1dd-dc71eb744b8f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                },
+                {
+                    "id": "576d6d5f-cceb-4676-bbed-b2eea73afac8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "b4acb497-2b9a-4638-80ff-d8ccf2ca8d73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "65114133-7980-4a4f-a2a9-209c1ef8c02d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                }
+            ],
+            "art_caption": "common",
+            "art_description": "Quality teach wall pressure."
+        }
+    ]
+}
